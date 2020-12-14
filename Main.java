6b7b67bf8afa33bb482317970616614a0a649178
KELVIN NGO <kngo001@mymail.lausd.net>
2020-12-14 08:40:36 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,70 @@
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Choose a number between 0-29: ");
+    int x = scan.nextInt();
+    System.out.println(x);
+    //user puts a number for a sports team and give you the placements
+    if (x == 0) {
+      System.out.println("Los Angeles Lakers, Ranked: 1st on Western, Win/Loss: 56-19");
+    }else if (x == 1 ){
+      System.out.println("Los Angeles Clippers, Ranked: 2nd on Western, Win/Loss: 49-23");
+    }else if (x == 2){
+      System.out.println("Denver Nuggets, Ranked: 3rd on Western, Win/Loss: 46-27");
+    }else if (x == 3){
+      System.out.println("Houstan Rockets, Ranked: 4th on Western, Win/Loss: 44-28");
+    }else if (x == 4){
+      System.out.println("Oklahoma Thunders, Ranked: 5th on Western, Win/Loss: 44-28");
+    }else if (x == 5){
+      System.out.println("Utah Jazz, Ranked: 6th on Western, Win/Loss: 44-28");
+    }else if (x == 6){
+      System.out.println("Dallas Mavericks, Ranked: 7th on Western, Win/Loss: 43-32");
+    }else if (x == 7){
+      System.out.println("Portland Trail Blazers, Ranked: 8th on Western, Win/Loss: 35-39");
+    }else if (x == 8){
+      System.out.println("Memphis Grizzles, Ranked: 9th on Western, Win/Loss: 34-39");
+    }else if (x == 9){
+      System.out.println("Phoenix Suns, Ranked: 10th on Western, Win/Loss: 34-39");
+    }else if (x == 10){
+      System.out.println("San Antonio Spurs, Ranked: 11th on Western, Win/Loss: 32-39");
+    }else if (x == 11){
+      System.out.println("Sacramento Kings, Ranked: 12th on Western, Win/Loss: 31-41");
+    }else if (x == 12){
+      System.out.println("New Orleans, Ranked: 13th on Western, Win/Loss: 30-42");
+    }else if (x == 13){
+      System.out.println("Minnesota Timberwolves, Ranked: 14th on Western, Win/Loss: 19-45");
+    }else if (x == 14){
+      System.out.println("Golden States Warriors, Ranked: 15th on Western, Win/Loss: 15-50");
+    }else if (x == 15){
+      System.out.println("Milwaukee Bucks, Ranked: 1st on Eastern, Win/Loss: 56-17");
+    }else if (x == 16){
+      System.out.println("Toronto Rapters, Ranked: 2nd on Eastern, Win/Loss: 53-19");
+    }else if (x == 17){
+      System.out.println("Boston Celtics, Ranked: 3rd on Eastern, Win/Loss: 48-24");
+    }else if (x == 18){
+      System.out.println("New Indiana, Ranked 4th on Eastern, Win/Loss: 45-28");
+    }else if (x == 19){
+      System.out.println("Miami Heats, Ranked: 5th on Eastern, Win/Loss: 44-29");
+    }else if (x == 20){
+      System.out.println("Philadelphia 76ers, Ranked: 6th on Eastern, Win/Loss: 43-30");
+    }else if (x == 21){
+      System.out.println("Brooklyn Nets, Ranked: 7th on Eastern, Win/Loss: 35-37");
+    }else if (x == 22){
+      System.out.println("Orlando Magic, Ranked: 8th on Eastern, Win/Loss: 33-40");
+    }else if (x == 23){
+      System.out.println("Washington Wizards, Ranked: 9th on Eastern, Win/Loss: 25-47");
+    }else if (x == 24){
+      System.out.println("Charlotte Hornets, Ranked: 10th on Eastern, Win/Loss: 23-42");
+    }else if (x == 25){
+      System.out.println("Chicago Bulls, Ranked: 11th on Eastern, Win/Loss: 22-43");
+    }else if (x == 26){
+      System.out.println("New York Knicks, Ranked: 12th on Eastern, Win/Loss: 21-45");
+    }else if (x == 27){
+      System.out.println("Denver Pistons, Ranked: 13th on Eastern, Win/Loss: 20-46");
+    }else if (x == 28){
+      System.out.println("Alanta Hawks, Ranked: 14th on Eastern, Win/Loss: 20-47");
+    }else if (x == 29){
+      System.out.println("Clevland Cavaliers, Ranked: 15th on Eastern, Win/Loss: 19-46");
+    }
+  }
+}
